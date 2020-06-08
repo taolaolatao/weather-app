f42f4742982ebb21a02e03db53e9b0bfc1d0c0da
VHM <taolaolataom@gmail.com>
2020-06-08 16:28:01 +0700

    Updates

---
+++ precache-manifest.d4d11c3661bfccb42223a57452f32b88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0d070ec0faaad6baea891972bf71d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211548675f87f80798fc",
+    "url": "/static/css/main.eac042a7.chunk.css"
+  },
+  {
+    "revision": "2fe1c56f8d6266ce9b24",
+    "url": "/static/js/2.5a8a5322.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a8a5322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211548675f87f80798fc",
+    "url": "/static/js/main.cf7f91b6.chunk.js"
+  },
+  {
+    "revision": "5cfe63f3d0aa65dc782b",
+    "url": "/static/js/runtime-main.dbafcd4d.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
